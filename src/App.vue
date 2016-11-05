7aefb19bddce7aab28a158d928cb8ea6429917cf
starkhorn <t.earendel@gmail.com>
2016-11-05 16:43:13 +0700

    Try canvas with Fabric.js

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,85 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
-  </div>
+<div id="app">
+  <button type="button" @click="toggleCreateRect">Create</button>
+
+  <my-canvas :width="1024" :height="768" :image="'http://img.friv5games.com/2016/03/21/police-station-floor-plans-l-48401def008c3ef9.jpg'" :command="currentCommand">
+  </my-canvas>
+</div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Canvas from './components/Canvas'
+import { fabric } from 'fabric'
 
 export default {
   name: 'app',
+
   components: {
-    Hello
+    'my-canvas': Canvas
+  },
+
+  data: () => ({
+    currentCommand: null
+  }),
+
+  methods: {
+    toggleCreateRect: function() {
+      this.currentCommand = (function(vm) {
+        let creatingRect = null
+        let origin = null
+
+        return {
+          init: function({ canvas }) {
+            canvas.selection = false
+
+            creatingRect = new fabric.Rect({
+              fill: 'green',
+              opacity: 0.2,
+              visible: false
+            })
+          },
+          mousedown: function({ e, canvas }) {
+            const existingObject = canvas.findTarget(e)
+            if (existingObject) {
+              return
+            }
+
+            origin = canvas.getPointer(e)
+            creatingRect.set({
+              visible: true
+            })
+
+            canvas.add(creatingRect)
+          },
+          mouseup: function({ e, canvas }) {
+            this.init({ canvas: canvas })
+          },
+          mousemove: function({ e, canvas }) {
+            if (!creatingRect.visible) {
+              return
+            }
+
+            const pointer = canvas.getPointer(e)
+            const dx = pointer.x - origin.x
+            const dy = pointer.y - origin.y
+
+            creatingRect.set({
+              left: origin.x + Math.min(0, dx),
+              top: origin.y + Math.min(0, dy),
+              width: Math.abs(dx),
+              height: Math.abs(dy)
+            })
+
+            creatingRect.setCoords()
+            canvas.renderAll()
+          }
+        }
+      })(this)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Canvas.vue
@@ -0,0 +1,70 @@
+<template lang="html">
+  <div>
+    <canvas ref="canvas" :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<script>
+import { fabric } from 'fabric'
+
+export default {
+  props: {
+    width: Number,
+    height: Number,
+    image: String,
+
+    command: Object
+  },
+
+  mounted: function() {
+    this.$nextTick(() => {
+      this.createCanvas()
+      this.setupCommandHandler()
+    })
+  },
+
+  methods: {
+    createCanvas: function() {
+      this.canvas = new fabric.Canvas(this.$refs.canvas, {
+        uniScaleTransform: true
+      })
+
+      fabric.Image.fromURL(this.image, (image) => {
+        image.scaleToWidth(this.canvas.width)
+
+        this.canvas.setBackgroundImage(image, this.canvas.renderAll.bind(this.canvas))
+      })
+    },
+
+    setupCommandHandler: function() {
+      this.$watch('command', function(command) {
+        if (this.command && this.command.init) {
+          this.command.init({ canvas: this.canvas })
+        }
+      })
+
+      this.canvas
+        .on('mouse:down', (e) => {
+          if (this.command) {
+            this.command.mousedown({ canvas: this.canvas, ...e })
+          }
+        })
+
+      this.canvas.on('mouse:up', (e) => {
+        if (this.command) {
+          this.command.mouseup({ canvas: this.canvas, ...e })
+        }
+      })
+
+      this.canvas.on('mouse:move', (e) => {
+        if (this.command) {
+          this.command.mousemove({ canvas: this.canvas, ...e })
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
